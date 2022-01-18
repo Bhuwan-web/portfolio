301378b54780cc8f672e1933c5f069ee99ce78b7
Bhuwan Panta <ricky.pantha@gmail.com>
2022-01-18 08:43:21 +0545

    stablized dp and back_dp uppload

--- profiles/views.py
+++ profiles/views.py
@@ -4,30 +4,56 @@ from django.urls import reverse_lazy
 from profiles.forms import User, UserInfoForm, UserProfileForm
 from profiles.models import UserProfile
 from django.shortcuts import redirect
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
 
-class UserProfileCreateView(CreateView, UpdateView):
-    model = UserProfile
-    form_class = UserProfileForm
-    template_name = "profiles/user_info.html"
-    success_url = reverse_lazy("profile:profile")
+# class UserProfileCreateView(CreateView, UpdateView):
+#     model = UserProfile
+#     form_class = UserProfileForm
+#     template_name = "profiles/user_info.html"
+#     success_url = reverse_lazy("profile:profile")
 
-    def get(self, request):
-        try:
-            self.model.objects.get(basic_info=self.request.user)
-        except:
-            self.model.objects.create(basic_info=self.request.user)
-        return super().get(request)
+#     def get(self, request):
+#         try:
+#             self.model.objects.get(basic_info=self.request.user)
+#         except:
+#             self.model.objects.create(basic_info=self.request.user)
+#         return super().get(request)
 
-    def get_object(self, queryset=None):
-        obj = self.model.objects.get(basic_info=self.request.user)
-        return obj
+#     def get_object(self, queryset=None):
+#         obj = self.model.objects.get(basic_info=self.request.user)
+#         return obj
 
-    def form_valid(self, form):
-        self.object = form.save()
-        return super().form_valid(form)
+#     def form_valid(self, form):
+#         """If the form is valid, save the associated model."""
+#         print(self.request.FILES)
+#         # dp = self.request.FILES["dp"]
+#         # back_dp = self.request.FILES["background_dp"]
+#         # fs = FileSystemStorage()
+#         # fs.save(dp.name, dp)
+#         # fs.save(back_dp.name, back_dp)
+#         self.object = form.save()
+#         return super().form_valid(form)
+
+
+def user_profile_edit_view(request):
+    try:
+        profile = UserProfile.objects.get(basic_info=request.user)
+    except:
+        profile = UserProfile.objects.create(basic_info=request.user)
+        profile.save()
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        files = request.FILES
+        posts = request.POST
+        print(files)
+        if form.is_valid():
+            form.save()
+            return redirect("profile:profile")
+    form = UserProfileForm(instance=profile)
+    return render(request, "profiles/edit_profile.html", {"form": form})
 
 
 class UserProfileView(DetailView):
@@ -44,7 +70,7 @@ class UserProfileView(DetailView):
 
 
 class UserInfoView(UpdateView):
-    template_name = "profiles/user_info.html"
+    template_name = "profiles/edit_profile.html"
     form_class = UserInfoForm
     model = User
     success_url = reverse_lazy("profile:profile")
